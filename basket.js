7427ffbef205d5d0bcdeda935c337593dd5945e5
MarkoM91 <markomedic91@gmail.com>
2019-03-07 15:47:59 +0100

    commit1

---
+++ basket.js
@@ -0,0 +1,82 @@
+function getRandom(min, max) {
+
+  return Math.floor(Math.random() * (max-min + 1)) + min;
+}
+
+function getRandomChar() {
+
+  var rndInt = getRandom(65, 90);
+  var rndChar = String.fromCharCode(rndInt);
+
+
+  return(rndChar);
+}
+
+function getRandomId() {
+  var rndId = "";
+
+  for (var i = 0; i < 3; i++) {
+
+  rndId += getRandomChar() + getRandom(0,9);
+  }
+console.log(rndId);
+}
+
+
+
+function getRandomPlayer() {
+
+  var twoPerc = getRandom(0, 100);
+  var threePerc = 100 - twoPerc -50;
+  var player = {
+
+  "id" : getRandomId(),
+  "points" : getRandom(0, 30),
+  "bounce" : getRandom(0, 15),
+  "mistake" :getRandom(0, 5),
+  "twoPerc" : twoPerc,
+  "threePerc" : threePerc
+};
+
+
+}
+
+function isPresent(player, players) {
+
+  var finded = false;
+
+  for (var i = 0; i < players.length; i++) {
+
+    if (player.id == players[i].id) {
+      finded = true;
+    }
+  }
+  return  finded;
+}
+
+function getRandomPlayers() {
+  var players = [];
+
+  for (var i = 0; i < 10; i++) {
+     var player = getRandomPlayer();
+    if (!isPresent(player, players)) {
+
+        players.push(getRandomPlayer());
+    }
+  }
+  return players;
+}
+
+
+
+
+
+function init() {
+console.log("ok");
+getRandomChar();
+getRandomId();
+getRandomPlayers();
+}
+
+
+$(document).ready(init);
